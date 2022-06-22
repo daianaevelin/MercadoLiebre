71ed4def7dba627dc0809284aa3748d7d99b18d2
daianaevelin <daiaanaevelin@hotmail.com>
2022-06-21 21:07:14 -0300

    agregue todo

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+//const path = require("path");
+
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "localhost";
+
+/* app.use(express.static(path.resolve(__dirname, "./public"))
+);
+ */
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + "/views/home.html");
+});
+
+/* app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+}); */
+
+app.get("/login", (req,res)=>{
+    res.sendFile(__dirname + "/views/login.html");
+});
+
+app.get("/register", (req,res)=>{
+    res.sendFile(__dirname + "/views/register.html");
+});
+
+app.listen(3000, () => {
+    console.log(`listening on http://${HOST}:${PORT}/`);
+});
